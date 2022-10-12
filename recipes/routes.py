3668160af7f728eef2dc4daeb19862b2d7a68fc4
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2022-10-12 22:03:41 +0000

    add login function and login.html

--- recipes/routes.py
+++ recipes/routes.py
@@ -27,7 +27,7 @@ def register():
             flash("Username already exists")
             return redirect(url_for("register"))
         
-        user =User(
+        user =Users(
             user_name=request.form.get("username").lower(),
             password=generate_password_hash(request.form.get("password"))
         )
@@ -35,11 +35,40 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        new_user = Users.query.get_or_404(user.id)
+
         session["user"] = request.form.get("username").lower()
         flash("Registration successful")
-        return redirect(url_for("profile", username=session["user"]))
+        return redirect(url_for("register", user_name=session["user"]))
 
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = Users.query.filter(
+            Users.user_name == request.form.get("username").lower()).all()
+
+        if existing_user:
+            print(request.form.get("username"))
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(request.form.get("username")))
+                        return redirect(url_for("login", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
