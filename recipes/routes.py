d973c6eaf77af0224f69120fe74c5208c9b85ce1
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2022-10-12 22:34:48 +0000

    add logout function

--- recipes/routes.py
+++ recipes/routes.py
@@ -81,3 +81,9 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
