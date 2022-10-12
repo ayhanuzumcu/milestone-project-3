e13b8bc1022eeef7bb9a18e5d646fc218141d6d9
ayhanuzumcu <ayhanuzumcu@hotmail.com>
2022-10-12 21:19:32 +0000

    create register page

--- recipes/routes.py
+++ recipes/routes.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 from recipes import app, db, mongo, mydb
 from recipes.models import Cuisine, Users
 
@@ -13,5 +14,32 @@ from recipes.models import Cuisine, Users
 def get_recipes():
     recipes = list(mydb.recipes.find())
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already registered in db
+        existing_user = Users.query.filter(
+            Users.user_name == request.form.get("username").lower()).all()
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        user =User(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
 
 
